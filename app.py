8ab7f865076eb4da64066e48d7168b59ce627d31
fuji <enlighten99@gmail.com>
2022-01-31 21:16:07 +0900

    kaminariページ追加

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask,render_template,session, request, redirect, url_for
 
+# インスタンスの作成
 app = Flask(__name__)
 
-@app.route("/")
+# メイン
+@app.route('/')
 def index():
     return 'Hello demo'
+
+@app.route('/kaminari')
+def kaminari():
+    return '雷鳴'
 
 ## 実行
 if __name__ == "__main__":
